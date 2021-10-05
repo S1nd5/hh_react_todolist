fadea7d00991979a661fdc0fb1395e9be85179b7
Santtu Lehtokari <35769108+S1nd5@users.noreply.github.com>
2021-10-05 17:35:56 +0300

    Exercises 20, 21 related to Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import Todos from './components/Todos';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import {
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 
 function App() {
+
+  const [value, setValue] = useState('home');
+  const handleChange = (event, value) => { setValue(value); };
+
   return (
     <div className="App">
-      <Todos/>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" style={{ fontWeight: 'bold' }} centered>
+          <Tab value="home" label="Home" />
+          <Tab value="todos" label="Todos" />
+        </Tabs>
+      </AppBar>
+      {value === 'home' && <div><h1>Welcome to the Todo App!</h1><p>Lorem ipsum dolor simet...</p></div>}
+      {value === 'todos' &&  <MuiPickersUtilsProvider utils={DateFnsUtils}><Todos /></MuiPickersUtilsProvider>}
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,16 @@
 import React, { useRef } from 'react';
-import TodoTable from './TodoTable';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+import {
+    DatePicker,
+} from '@material-ui/pickers';
 
 const columns = [
     { field: 'text', sortable: true, filter: true, floatingFilter: true },
@@ -11,45 +18,72 @@ const columns = [
     { field: 'priority', sortable: true, filter: true, floatingFilter: true, cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' } },
 ]
 
+function convertDate(date) {
+    return (date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear());
+}
+
 function Todos() {
 
-    const [desc, setDesc] = React.useState({ text: '', date: '', priority: '' });
+    const [selectedDate, setDate] = React.useState(new Date());
+    const [desc, setDesc] = React.useState({ text: '', date: convertDate(selectedDate), priority: '' });
     const [todos, setTodos] = React.useState([]);
 
     const gridRef = useRef();
 
-    const addTodo = () => {
-        setTodos([...todos, desc]);
+    const handleDate = (date) => {
+        setDate(date);
+        setDesc({ ...desc, date: convertDate(date) });
+    }
+
+    const addTodo = (event) => {
+        event.preventDefault();
+        if (desc.text.length < 3 || desc.priority.length < 3) {
+            alert("Please give a description for the task!");
+        } else {
+            setTodos([...todos, desc]);
+        }
     }
 
     const removeTodo = () => {
         if (gridRef.current.getSelectedNodes().length > 0) {
             setTodos(todos.filter((todo, i) => i !== gridRef.current.getSelectedNodes()[0].childIndex));
         } else {
-            alert("No row selected!")
+            alert("No row selected!");
         }
     }
+
+    const priorities = [
+        { value: "Low", label: "Low" },
+        { value: "Normal", label: "Normal" },
+        { value: "High", label: "High" }
+    ]
 
     return (
         <div>
             <h1>Simple Todolist</h1>
             <h3>Add new todo</h3>
-            <div id="add">
-                <label>Description</label>
-                <input value={desc.text} onChange={e => setDesc({ ...desc, text: e.target.value })} />
-            </div>
-            <div id="add">
-                <label>Date</label>
-                <input type="date" value={desc.date} onChange={e => setDesc({ ...desc, date: e.target.value })} />
-            </div>
-            <div id="add">
-                <label>Priority</label>
-                <input type="text" value={desc.priority} onChange={e => setDesc({ ...desc, priority: e.target.value })} />
-                <button onClick={addTodo}>Add</button>
-                <button onClick={removeTodo}>Delete</button>
-            </div>
+            <Stack spacing={2} direction="row" justifyContent="center">
+                <TextField label="Description" placeholder="Write here..." variant="standard" value={desc.text} onChange={e => setDesc({ ...desc, text: e.target.value })} />
+                <DatePicker label="Date" variant="standard" value={selectedDate} onChange={date => { handleDate(date) }} />
+                <TextField
+                    select
+                    label="Priority"
+                    variant="standard"
+                    value={desc.priority}
+                    style={{ width: '200px' }}
+                    onChange={e => setDesc({ ...desc, priority: e.target.value })}
+                >
+                    {priorities.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <Button onClick={addTodo} variant="contained">Add</Button>
+                <Button onClick={removeTodo} variant="contained" color="error">Delete</Button>
+            </Stack>
             <h3>Content</h3>
-            <div className="ag-theme-material" id="add" style={{ height: '400px', minWidth: '50vh' }}>
+            <div className="ag-theme-material" id="add" style={{ height: '800px', minWidth: '50vh' }}>
                 <AgGridReact ref={gridRef} onGridReady={params => gridRef.current = params.api} rowSelection="single" animateRows={true} columnDefs={columns} rowData={todos}>
                 </AgGridReact>
             </div>
